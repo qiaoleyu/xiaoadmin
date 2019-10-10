2fb1651cfbd4462a2d2c86b67159af9d2ffed003
qiaoleyu <1026573472@qq.com>
2019-10-10 21:50:37 +0800

    a

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -41,7 +41,7 @@
         </div>
       </el-header>
 
-        <el-container style="height: 500px; border: 1px solid #eee">
+        <el-container style="height: 600px; border: 1px solid #eee">
           <!--<el-aside width="200px" style="background-color: rgb(238, 241, 246)">-->
             <el-aside class="app-side app-side-left"
             style="width: inherit;">
