f964911a88a9a27ef103e1f22b09fc677fcbb0c2
wangqingq <2423668851@qq.com>
2019-10-13 13:18:20 +0800

    部署服务器

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8082/', // 设置你调用的接口域名和端口号
+        target: 'http://121.40.115.218:8080/xiaoMi/', // 设置你调用的接口域名和端口号
         changeOrigin: true,   // 跨域
         pathRewrite: {
           '^/api': '/'
